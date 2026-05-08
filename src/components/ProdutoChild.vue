b12f3b1b588f9c09e72cddc8ea0c449c80e6bb61
JoaoHBB2010 <joaohenriquebatista.2010@gmail.com>
2026-05-08 14:02:51 -0300

    first commit

---
+++ src/components/ProdutoChild.vue
@@ -0,0 +1,16 @@
+<script setup>
+import ButtonChild from './ButtonChild.vue';
+defineProps(['id','nome','preco','categoria']);
+defineEmits(['corrigirpreco']);
+import { formataPreco } from '@/utils/produtoUtils';
+</script>
+
+<template>
+    <li>
+        ID: {{ id }} - {{ nome }} - R$ {{ formataPreco(preco) }} ({{ categoria }})
+        <ButtonChild @clique="$emit('corrigirpreco',id)">Corrigir Preço</ButtonChild>
+    </li>
+</template>
+
+<style scoped>
+</style>
